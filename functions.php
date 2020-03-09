e162cd4eb5568393814cb69b8bc880ae88c3bc77
David DOGNIN <dd08.brad@hotmail.fr>
2020-03-09 19:53:26 +0100

    correction problème avec fichier css

--- functions.php
+++ functions.php
@@ -8,8 +8,8 @@ add_theme_support('title-tag');
 
 function init_styles()
 {
-    wp_enqueue_style('normalize', get_template_directory_uri().'normalize.css');
-    wp_enqueue_style('style', get_template_directory_uri().'style.css');
+    wp_enqueue_style('normalize', get_template_directory_uri().'/normalize.css');
+    wp_enqueue_style('style', get_template_directory_uri().'/style.css');
 }
 
 add_action('wp_enqueue_scripts', 'init_styles');
