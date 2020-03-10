c9e59c0d4791e2dafb552e5bdcddd3ba2a9e20f1
David-49 <59173768+David-49@users.noreply.github.com>
2020-03-10 14:18:42 +0100

    maj

--- footer.php
+++ footer.php
@@ -1,33 +1,45 @@
 <footer class="footer">
     <div class="langue">
-        <img src="<?php echo get_template_directory_uri(); ?>/img/france.svg" alt="Logo" alt="logo langue française" class="drapeau">
-        <img src="<?php echo get_template_directory_uri(); ?>/img/uk.svg" alt="Logo" alt="logo langue française" class="drapeau">
-        <img src="<?php echo get_template_directory_uri(); ?>/img/spain.svg" alt="Logo" alt="logo langue française" class="drapeau">
+        <img src="<?php echo get_template_directory_uri(); ?>/img/france.svg"  alt="logo langue française" class="drapeau">
+        <img src="<?php echo get_template_directory_uri(); ?>/img/uk.svg"  alt="logo langue française" class="drapeau">
+        <img src="<?php echo get_template_directory_uri(); ?>/img/spain.svg"  alt="logo langue française" class="drapeau">
     </div>
+
+    <div class="ligneSeparation"></div>
 
     <div class="moyenPaiement">
         <h3 class="titreH3">Moyens de paiement</h3>
         <p class="descMoyen">Moyens de paiement sécurisés</p>
         <div class="logoPaiement">
-            <img src="<?php echo get_template_directory_uri(); ?>/img/visa.svg" alt="Logo" alt="logo langue française" class="paiement visa">
-            <img src="<?php echo get_template_directory_uri(); ?>/img/paypal.svg" alt="Logo" alt="logo langue française" class="paiement paypal">
-            <img src="<?php echo get_template_directory_uri(); ?>/img/mastercard.svg" alt="Logo" alt="logo langue française" class="paiement mastercard">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/visa1.svg" alt="Logo mode de paiement (visa)" class="paiement visa">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/paypal1.svg" alt="Logo mode de paiement (paypal)" class="paiement paypal">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/mastercard1.svg" alt="Logo mode de paiement (mastercard)" class="paiement mastercard">
         </div>
     </div>
+
+    <div class="ligneSeparation"></div>
 
     <div class="moyenLivraison">
         <h3 class="titreH3">Moyen de livraison</h3>
         <p class="descMoyen">Livraison rapide</p>
-        <img src="<?php echo get_template_directory_uri(); ?>/img/fedex.svg" alt="" class="logoFedex">
-        <p class="descLivraison">Qualité de service certifiée</p>
-        <p class="descLivraison">Qualité des produits prouvés</p>
+        <img src="<?php echo get_template_directory_uri(); ?>/img/fedex.svg" alt="logo de moyen de livraison (Fedex)" class="logoFedex">
+        <p class="descLivraison p1-descLivraison ">Qualité de service certifiée</p>
+        <p class="descLivraison p2-descLivraison">Qualité des produits prouvés</p>
     </div>
+
+    <div class="ligneSeparation"></div>
 
     <div class="moyenContact">
         <h3 class="titreH3">Contactez-nous</h3>
         <p class="descMoyen">Email, réseaux sociaux</p>
         <div class="blocMail">
             <p class="mailSite">Piikti.contact@gmail.com</p>
+            <img src="<?php echo get_template_directory_uri(); ?>/img/mail.svg" class="logoMail">
+        </div>
+        <div class="logoRS">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/facebook.svg" alt="Logo" alt="logo réseaux sociaux" class="reseauSociaux">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/instagram.svg" alt="Logo" alt="logo réseaux sociaux" class="reseauSociaux">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/pinterest.svg" alt="Logo" alt="logo réseaux sociaux" class="reseauSociaux">
         </div>
 
     </div>
